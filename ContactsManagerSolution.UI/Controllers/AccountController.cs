9aa0020b88b830c76fb1036335b5b60a728a9533
Imon Islam <imon0077@yahoo.com>
2023-10-14 10:03:29 +0600

    Migration added for IdentityTables, cmpleted Register post method to insert a user into db along with validation.

--- ContactsManagerSolution.UI/Controllers/AccountController.cs
+++ ContactsManagerSolution.UI/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using ContactsManager.Controllers;
+using ContactsManager.Core.Domain.IdentityEntities;
 using ContactsManager.Core.DTO;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsManager.UI.Controllers
@@ -7,6 +9,14 @@ namespace ContactsManager.UI.Controllers
     [Route("[controller]/[action]")]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -14,11 +24,37 @@ namespace ContactsManager.UI.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterDTO registerDTO)
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
         {
-            //TO DO: Store user registration details into Identity database
+            //Check for validation error
+            if(ModelState.IsValid == false)
+            {
+                ViewBag.Error = ModelState.Values.SelectMany(temp => temp.Errors).Select(temp => temp.ErrorMessage);
+                return View(registerDTO);
+            }
 
-            return RedirectToAction(nameof(PersonsController.Index), "Persons");
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerDTO.Email,
+                PhoneNumber = registerDTO.Phone,
+                PersonName = registerDTO.PersonName,
+                UserName = registerDTO.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction(nameof(PersonsController.Index), "Persons");
+            }
+            else
+            {
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+                return View(registerDTO);
+            }
         }
     }
 }
